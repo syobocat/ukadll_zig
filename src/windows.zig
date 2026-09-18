47394e0fa8ba4e4c88cab85e85e3746dad66f098
SyoBoN <syobon@syobon.net>
2026-09-18 18:11:45 +0900

    support macos

---
+++ src/windows.zig
@@ -0,0 +1,113 @@
+// SPDX-FileCopyrightText: 2025-2026 SyoBoN <syobon@syobon.net>
+//
+// SPDX-License-Identifier: UPL-1.0
+
+const std = @import("std");
+const builtin = @import("builtin");
+const win32 = @import("win32");
+
+const GMEM_FIXED = win32.system.memory.GMEM_FIXED;
+const globalAlloc = win32.system.memory.GlobalAlloc;
+const globalFree = win32.system.memory.GlobalFree;
+
+pub fn request(gpa: std.mem.Allocator, comptime f: fn (std.mem.Allocator, []const u8) [:0]const u8) fn (*anyopaque, *c_long) callconv(.c) *anyopaque {
+    return struct {
+        fn request(h: *anyopaque, len: *c_long) callconv(.c) *anyopaque {
+            defer _ = globalFree(@intCast(@intFromPtr(h)));
+
+            var arena = std.heap.ArenaAllocator.init(gpa);
+            defer arena.deinit();
+
+            const allocator = arena.allocator();
+
+            const body = voidToString(h, len.*);
+            const res = f(allocator, body);
+
+            const addr: usize = @intCast(globalAlloc(GMEM_FIXED, res.len + 1)); // sentinel込み
+            const ptr: [*]u8 = @ptrFromInt(addr);
+            @memcpy(ptr, res);
+            ptr[res.len] = 0; // 念のため手動で0終端
+            len.* = @intCast(res.len); // lenにはsentinelを含まない
+            return @ptrCast(ptr);
+        }
+    }.request;
+}
+
+fn request_test(_: std.mem.Allocator, req: []const u8) [:0]const u8 {
+    std.log.info("Received a request: {s}\n", .{req});
+    return "OK";
+}
+test "Check request" {
+    const allocator = std.testing.allocator;
+
+    const len = 3;
+    var c_len: c_long = len;
+    const addr: usize = @intCast(globalAlloc(GMEM_FIXED, len + 1));
+    const ptr: [*]u8 = @ptrFromInt(addr);
+    @memcpy(ptr, "GET");
+    ptr[len] = 0;
+
+    const r = request(allocator, request_test);
+    const res = r(ptr, &c_len);
+    const res_str = voidToString(res, 2);
+    std.log.info("Received a response: {s}\n", .{res_str});
+}
+
+pub fn load(comptime f: fn ([]const u8) anyerror!void) fn (*anyopaque, c_long) callconv(.c) c_int {
+    return struct {
+        fn load(h: *anyopaque, len: c_long) callconv(.c) c_int {
+            defer _ = globalFree(@intCast(@intFromPtr(h)));
+
+            const str = voidToString(h, len);
+            f(str) catch return boolToInt(false);
+            return boolToInt(true);
+        }
+    }.load;
+}
+
+fn load_test(v: []const u8) !void {
+    std.log.info("{s}\n", .{v});
+}
+test "Check load" {
+    const l = load(load_test);
+
+    const len = 13;
+    const addr: usize = @intCast(globalAlloc(GMEM_FIXED, len + 1));
+    const ptr: [*]u8 = @ptrFromInt(addr);
+    @memcpy(ptr, "Hello, World!");
+    ptr[len] = 0;
+
+    try std.testing.expect(l(ptr, len) == 1);
+}
+
+pub fn unload(comptime f: fn () void) fn () callconv(.c) c_int {
+    return struct {
+        fn unload() callconv(.c) c_int {
+            f();
+            return boolToInt(true);
+        }
+    }.unload;
+}
+
+fn unload_test() void {
+    std.log.info("Goodbye, World!\n", .{});
+}
+test "Check unload" {
+    const ul = unload(unload_test);
+
+    try std.testing.expect(ul() == 1);
+}
+
+fn voidToString(h: *anyopaque, len: c_long) []const u8 {
+    const z_len: usize = @intCast(len);
+    const ptr: [*]const u8 = @ptrCast(h);
+    return ptr[0..z_len];
+}
+
+fn boolToInt(b: bool) c_int {
+    if (b) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
